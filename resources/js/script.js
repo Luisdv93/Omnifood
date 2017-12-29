c32457e22f5169d98b8efda2d4c3f70a899ec9e3
Luisdv93 <luisdv93@gmail.com>
2017-12-28 22:54:58 -0400

    add animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,90 @@
-$('.js-section-features').waypoint(function(direction){
-  if (direction === "down") {
-    $('nav').addClass('sticky');
-  } else {
-    $('nav').removeClass('sticky');
-  }
-}, {
-  offset: '120px'
+$(document).ready(function () {
+  /* Sticky Navigation */
+  $('.js-section-features').waypoint(function (direction) {
+    if (direction === "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+      offset: '120px'
+    });
+
+  /* Button Scroll */
+  $('.js-scroll-to-plan').click(function () {
+    $('html, body').animate({scrollTop: $('.js-section-plans').offset().top}, 2000);
+  });
+
+  $('.js-scroll-to-start').click(function () {
+    $('html, body').animate({scrollTop: $('.js-section-features').offset().top}, 1000);
+  });
+
+  /* Animations on scroll */
+  $('.js-wp-1').waypoint(function(direction){
+    $('.js-wp-1').addClass('animated fadeIn');
+  }, {
+    offset: '70%'
+  });
+
+  $('.js-wp-2').waypoint(function(direction){
+    $('.js-wp-2').addClass('animated zoomIn');
+  }, {
+    offset: '60%'
+  });
+
+  $('.js-wp-3').waypoint(function(direction){
+    $('.js-wp-3').addClass('animated slideInUp');
+  }, {
+    offset: '60%'
+  });
+
+  $('.js-wp-4').waypoint(function(direction){
+    $('.js-wp-4').addClass('animated fadeIn');
+  }, {
+    offset: '70%'
+  });
+
+  $('.js-wp-5').waypoint(function(direction){
+    $('.js-wp-5').addClass('animated wobble');
+  }, {
+    offset: '50%'
+  });
+
+  /* Navigation Scroll */
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
 });
